5f7d8892d7be3ab6d3464c31e5c9d4212ea0bfc2
switux <github@datamancers.net>
2025-01-08 21:40:13 +0100

    Fix code smell

--- src/alpaca_server.h
+++ src/alpaca_server.h
@@ -40,8 +40,7 @@ using configured_device_t = struct {
 	etl::string<32>	DeviceName;
 	etl::string<32>	DeviceType;
 	uint32_t		DeviceNumber;
-	etl::string<36>	UniqueID[36];
-
+	etl::string<36>	UniqueID;
 };
 
 enum struct ascom_error_t : byte
